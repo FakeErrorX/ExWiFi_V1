cb5dd9e781c0ed46db63cd425639ccb4314b88a1
ErrorX <99178547+ImErrorX@users.noreply.github.com>
2022-03-19 08:19:09 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# TEAM ERROR
+### ExWiFi_V1
+### Note:Your Device Must Be Rooted.
+  
+    Installation:
+
+```
+$ pkg update && pkg upgrade
+
+$ pkg install git python
+
+$ git clone https://github.com/ImErrorX/ExWiFi_V1
+
+$ cd ExWiFi_V1
+
+$ python V1.py
+```
+# ExWiFi Repo
+
+https://github.com/ImErrorX/ExWiFi
---
+++ V1.py
@@ -0,0 +1,25 @@
+import os
+os.system('cls||clear')
+print('''\033[1;92m❥══════════❥ ↓★↑Cᴏᴅᴇ↓★↑BY↓★↑EʀʀᴏʀX↓★❥══════════❥
+\033[0m
+\033[1;91m███████╗██╗░░██╗░██╗░░░░░░░██╗██╗███████╗██╗\033[0m
+\033[1;92m██╔════╝╚██╗██╔╝░██║░░██╗░░██║██║██╔════╝██║\033[0m
+\033[1;93m█████╗░░░╚███╔╝░░╚██╗████╗██╔╝██║█████╗░░██║\033[0m
+\033[1;94m██╔══╝░░░██╔██╗░░░████╔═████║░██║██╔══╝░░██║\033[0m
+\033[1;95m███████╗██╔╝╚██╗░░╚██╔╝░╚██╔╝░██║██║░░░░░██║\033[0m
+\033[1;96m╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░░░░╚═╝\033[0m
+''')
+print('''\033[1;91m❥═════════❥ ↓★↑D̶I̶S̶C̶O̶R̶D̶:̶!ErrorX#0001↓❥═════════❥\033[0m''')
+
+print('''\033[1;36;40mExWiFi Installer By ErrorX
+Your Device Must Be Rooted
+If Any Question,
+Contact Me On Discord
+Discord:! ErrorX#0001 \n''')
+os.system("pkg install -y root-repo")
+os.system("pkg install -y git tsu python wpa-supplicant pixiewps iw")
+os.system("cd ..;git clone https://github.com/ImErrorX/ExWiFi")
+
+os.system("cd ..;chmod +x ExWiFi/ErrorX.py")
+
+print("\033[1;34;40mThanks.\nInstallation Done.\nNow Go To Home Directory[~] And Enter This Command :\nsudo python ExWiFi/ErrorX.py -i wlan0 -K")
